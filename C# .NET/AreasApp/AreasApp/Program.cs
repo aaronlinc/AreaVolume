c141ee4f7b83c25e968c4607896d677ca97523ce
Aaron Lincoln <aaronlinc@gmail.com>
2015-10-23 15:19:05 -0700

    First commit, started area class

---
+++ C# .NET/AreasApp/AreasApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreasApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Area
+    {
+        public float square(float height, float width)
+        {
+            return (height * width);
+        }
+
+        public double circle(double radius)
+        {
+            return ((Math.PI) * (radius * radius));
+        }
+
+    }
+}
